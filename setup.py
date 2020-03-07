f8cd16a0f6da6328b681eda12b1c4668079f0021
Willy Raedy <wraedy@gmail.com>
2020-03-07 11:41:39 -0600

    commit cookie cutter data structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='How can maximize the visibility of climate change news on Reddit?',
+    author='Willy Raedy',
+    license='MIT',
+)
